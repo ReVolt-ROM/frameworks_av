dfd4b4612dba25b35212772bee2008b219299702
John Hany <johnhany97@gmail.com>
2013-11-20 14:50:17 -0500

    Revert "Merge github.com:AOKP/frameworks_av into HEAD"
    This reverts commit bc0fa50f7f252a425cf7711348585b6b450025f4, reversing
    changes made to fd938133e89a85785f93a2e9c56f5606eae865c6.

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -30,10 +30,6 @@ ifeq ($(BOARD_HTC_3D_SUPPORT),true)
    LOCAL_CFLAGS += -DHTC_3D_SUPPORT
 endif
 
-ifeq ($(BOARD_HTC_3D_SUPPORT),true)
-   LOCAL_CFLAGS += -DHTC_3D_SUPPORT
-endif
-
 LOCAL_SRC_FILES:=                         \
         ACodec.cpp                        \
         AACExtractor.cpp                  \
